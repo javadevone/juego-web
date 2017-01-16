1374f8cb64aa2099f092b9e88c68aca639cc3af0
javadevone <javadevone@gmail.com>
2017-01-16 17:17:22 +0100

    Episodio 5
    El archivo index.php ahora tiene un contenido básico que muestra una página simple.

--- juego-web/index.php
+++ juego-web/index.php
@@ -1,1 +1,14 @@
-¡Hola suscriptores!
+<!doctype html>
+
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<title>Juego web</title>
+		<meta name="description" conent="Esto es un juego web">
+		<meta name="author" conent="JavaDevOne">
+	</head>
+	<body>
+		<h1>Bienvenido al canal de JavaDevOne</h1>
+		<h3>Recordad que el canal tiene cuenta en Patreon</h3>
+	</body>
+</html>
