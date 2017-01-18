8ff73e243771cb7d4924b3417078af5315814f51
javadevone <javadevone@gmail.com>
2017-01-18 18:24:53 +0100

    Episodio 6
    Hemos añadido una carpeta js para los archivos Javascript y el primer archivo, inicio.js, que se ocupa de iniciar la ejecución del código.

--- juego-web/index.php
+++ juego-web/index.php
@@ -10,5 +10,6 @@
 	<body>
 		<h1>Bienvenido al canal de JavaDevOne</h1>
 		<h3>Recordad que el canal tiene cuenta en Patreon</h3>
+		<script src="js/inicio.js"></script>
 	</body>
 </html>
---
+++ juego-web/js/inicio.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+	console.log("Javascript funciona");
+}, false);
+
+//DOM = DOCUMENT OBJECT MODEL
