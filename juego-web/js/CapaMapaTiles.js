5331b66525755e1e9ea8416706d0bcb3f6cee53f
JavaDevOne <javadevone@gmail.com>
2017-03-14 23:31:17 +0100

    Episodio 24
    Se ha añadido el archivo que maneja las capas de tiles del mapa

---
+++ juego-web/js/CapaMapaTiles.js
@@ -0,0 +1,31 @@
+function CapaMapaTiles(datosCapa, indiceZ, anchoDeLosTiles, altoDeLosTiles, paletaSprites) {
+	this.anchoEnTiles = parseInt(datosCapa.width);
+	this.altoEnTiles = parseInt(datosCapa.height);
+	this.x = parseInt(datosCapa.x);
+	this.y = parseInt(datosCapa.y);
+	this.z = indiceZ;
+	this.tiles = [];
+
+	for (y = 0; y < this.altoEnTiles; y++) {
+		for (x = 0; x < this.anchoEnTiles; x++) {
+			var idSpriteActualSobreUno = datosCapa.data[x + y * this.anchoEnTiles];
+			if (idSpriteActualSobreUno == 0) {
+				this.tiles.push(null);
+			} else {
+				var spriteActual = this.encontrarSpriteEnPaletaPorId(idSpriteActualSobreUno - 1, paletasSprites);
+
+				this.tiles.push(new Tile(x, y, indiceZ, anchoDeLosTiles, altoDeLosTiles, spriteActual));
+			}
+		}
+	}
+}
+
+CapaMapatiles.prototype.encontrarSpriteEnPaletaPorId = function(idSpriteSobreZero, paletasSprites) {
+	for (s = 0; s < paletasSprites.length; s++) {
+		if (idSpriteSobreZero >= paletasSprites[s].primerSpriteSobreUno - 1 &&
+			idSpriteSobreZero < paletasSprites[s].totalSprites + paletasSprites[s].primerSpriteSobreUno + 1) {
+			return paletasSprites[s].sprites[Math.abs(paletasSprites[s].primerSpriteSobreUno - 1 - idSpriteSobreZero)];
+		}
+	}
+	throw "El ID sobre ZERO " + idSpriteSobreZero + " del sprite no existe en ninguna paleta";
+}
--- juego-web/js/Sprite.js
+++ juego-web/js/Sprite.js
@@ -1,6 +1,6 @@
 function Sprite(ruta, idSobreZero, posicionEnHoja) {
 	var elementosRuta = ruta.split("/");
-	this.rutaHojaOrigen = "img/" elementosRuta[elementosRuta.length - 1];
+	this.rutaHojaOrigen = "img/" + elementosRuta[elementosRuta.length - 1];
 	this.idSobreZero = idSobreZero;
 	this.idSobreUno = idSobreZero + 1;
 	this.posicionEnHoja = posicionEnHoja;
